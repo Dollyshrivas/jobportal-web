6b8cef51b262f2520eecf3d344ab870ad30544a0
Dolly <dollyshrivas2005@gmail.com>
2026-07-26 15:50:03 +0530

    Delete middleware directory

--- middleware/protectRoute.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-  if (!req.session.user) {
-    req.flash("error", "Please login to access this page");
-    return res.redirect("/loginpage");
-  }
-  next();
-};
